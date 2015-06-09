da30b825225f57b021bcab964df88cb01f4fc7b0
bairamsa <bairamsa@INbairamsa.local>
2015-06-09 20:45:57 +0530

    size of choosen object changes with slider value

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -1,3 +1,4 @@
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -10,30 +11,43 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    console.log(data);
+    
+    console.log("data: "+data);
 
-    if (data==="drag1" || data==="drag2" || data==="drag3" || data==="drag4"){
+    if (data === "drag1" || data === "drag2" || data === "drag3" || data === "drag4"){
             console.log("in 1");
         ev.target.appendChild(document.getElementById(data));
-        var ob1=document.getElementById("drag1").setAttribute("draggable",false);  
-        var ob2=document.getElementById("drag2").setAttribute("draggable",false);
-        var ob3=document.getElementById("drag3").setAttribute("draggable",false);
-        var ob4=document.getElementById("drag4").setAttribute("draggable",false);
-        document.getElementById(data).setAttribute("draggable",true);
+        var ob1=document.getElementById("drag1").setAttribute("draggable", false);  
+        var ob2=document.getElementById("drag2").setAttribute("draggable", false);
+        var ob3=document.getElementById("drag3").setAttribute("draggable", false);
+        var ob4=document.getElementById("drag4").setAttribute("draggable", false);
+        document.getElementById(data).setAttribute("draggable", true);
 //       var target=((data==="drag1")?"drag1":((data==="drag2")?"drag2":((data==="drag3")?"drag3":"drag4")));
 //    console.log("target: "+target);
 //    var ob5=document.getElementById(ev.target.id).setAttribute("draggable",true);
 //    console.log(document.getElementById("div1"));
 //    var target=((data===ob1)?ob1:((data===ob2)?ob2:(data===ob3)?ob3:ob4));
     }
-    else if (data==="drag11" || data==="drag22" || data==="drag33" || data==="drag44"){
+    else if (data === "drag11" || data === "drag22" || data === "drag33" || data === "drag44"){
         ev.target.appendChild(document.getElementById(data));
-        var ob1=document.getElementById("drag11").setAttribute("draggable",false);  
-        var ob2=document.getElementById("drag22").setAttribute("draggable",false);
-        var ob3=document.getElementById("drag33").setAttribute("draggable",false);
-        var ob4=document.getElementById("drag44").setAttribute("draggable",false);
-        document.getElementById(data).setAttribute("draggable",true);
+        var ob11=document.getElementById("drag11").setAttribute("draggable", false);  
+        var ob22=document.getElementById("drag22").setAttribute("draggable", false);
+        var ob33=document.getElementById("drag33").setAttribute("draggable", false);
+        var ob44=document.getElementById("drag44").setAttribute("draggable", false);
+        document.getElementById(data).setAttribute("draggable", true);
     }
     else
     {;}
+  droppedObj=data;
+  setLocalData("droppedObj", document.getElementById(data));
+}
+
+
+function setLocalData(tableDataName,tableData){
+        if(typeof(Storage) !== "undefined") {
+            localStorage.setItem(tableDataName,tableData);
+} else {
+    // Sorry! No Web Storage support..
+    console.log("no Support for storage");
+}
 }
---
+++ js/slider.js
@@ -0,0 +1,45 @@
+//var i = document.getElementById("slider"),
+//    o = document.querySelector("output");
+//
+//o.innerHTML = i.value;
+//
+//i.addEventListener('change', function () {
+//  o.innerHTML = i.value;
+//  console.log("value: "+i.value);
+//}, false);
+//
+//
+
+function sliderChange(value){
+//var i = document.getElementById("slider").value;
+console.log("value: "+value);
+
+var ld = document.getElementById("leftDestination");
+console.log("ld: "+ld);
+    
+document.getElementById("leftMass").value=value;
+//var droppedObj=getLocalData("getLocalData");
+console.log("op: "+document.getElementById(droppedObj));
+
+var droppedImg = document.getElementById(droppedObj);
+console.log("target: "+droppedObj);
+if(droppedImg) {
+    console.log("im in");
+    droppedImg.style.height = (value/35+40)+'px';
+    droppedImg.style.width = (value/35+40)+'px';
+
+}
+}
+
+
+function getLocalData(tableData)
+{
+    if(typeof(Storage) !== "undefined") {
+    return localStorage.getItem(tableData);
+} else {
+    // Sorry! No Web Storage support..
+    console.log("no Support for storage");
+}
+}
+
+
