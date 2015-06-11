033d62c5f15c59e2299e9360c34ec683896b5657
bairamsa <bairamsa@INbairamsa.local>
2015-06-11 19:31:06 +0530

    table generation works fine

--- js/canvas.js
+++ js/canvas.js
@@ -15,30 +15,33 @@ function init(){
 function moveDown(){
     if(parseInt(imgObj.style.top)<421)
         {
-        console.log("in l");
         imgObj.style.top = parseInt(imgObj.style.top) + speed + 'px';
         setTimeout(moveDown,60);
         }
     else
         {
-        console.log("obj: "+elementChildren(imgObj)[1].src.substring(30,elementChildren(imgObj)[1].src.length-4));
-        console.log("mass: "+document.getElementById("sliderLeft").value);
-
+//        console.log("obj: "+elementChildren(imgObj)[1].src.substring(30,elementChildren(imgObj)[1].src.length-4));
+//        console.log("mass: "+document.getElementById("sliderLeft").value);
         console.log("env: "+getEnv());
+        var el=elementChildren(imgObj)[1].src.substring(30,elementChildren(imgObj)[1].src.length-4);
+        var mass=document.getElementById("sliderLeft").value;
+        printExperimentData(el, mass, getEnv(), 10);
         }
 }
 
 function moveDownRight(){
     if(parseInt(imgObjRight.style.top)<420)
         {
-        console.log("in r");
         imgObjRight.style.top = parseInt(imgObjRight.style.top) + speedRight + 'px';
         setTimeout(moveDownRight,60);
         }
     else{
-        console.log(elementChildren(imgObjRight)[2].src.substring(30,elementChildren(imgObj)[1].src.length-4));
-        console.log("mass: "+document.getElementById("sliderRight").value);
-        console.log("env: "+getEnv());
+//        console.log(elementChildren(imgObjRight)[2].src.substring(30,elementChildren(imgObj)[1].src.length-4));
+//        console.log("mass: "+document.getElementById("sliderRight").value);
+//        console.log("env: "+getEnv());
+        var el=elementChildren(imgObjRight)[2].src.substring(30,elementChildren(imgObj)[1].src.length-4);
+        var mass=document.getElementById("sliderRight").value;
+        printExperimentData(el, mass, getEnv(), 10);
     }
 }
 var speedRight;
@@ -53,6 +56,7 @@ function dropObject(){
     var btn=document.getElementById("submit");
     btn.value="reset";
     btn.onclick=doRestore;
+    
 }
 
 
@@ -60,8 +64,8 @@ function doRestore(){
     console.log("imgObj: "+imgObj);
     console.log(elementChildren(imgObj)[1]);
     console.log("woh: "+imgObj.childNodes[1][0]);
-    elementChildren(imgObj)[1].width="50px";
-    elementChildren(imgObj)[1].height="50px";
+    elementChildren(imgObj)[1].style.width="50px";
+    elementChildren(imgObj)[1].style.height="50px";
     imgObjRight.style.width="50px";
     imgObjRight.style.height="50px";
     if(elementChildren(imgObj)[1] || elementChildren(imgObjRight)[2])
@@ -75,6 +79,14 @@ function doRestore(){
 
     imgObjRight.style.top = '0px';
     var btn=document.getElementById("submit").value="Drop";
+    document.getElementById("drag1").setAttribute("draggable", true);
+    document.getElementById("drag2").setAttribute("draggable", true);
+    document.getElementById("drag3").setAttribute("draggable", true);
+    document.getElementById("drag4").setAttribute("draggable", true);
+    document.getElementById("drag11").setAttribute("draggable", true);
+    document.getElementById("drag22").setAttribute("draggable", true);
+    document.getElementById("drag33").setAttribute("draggable", true);
+    document.getElementById("drag44").setAttribute("draggable", true);
 }
 
 
@@ -103,4 +115,19 @@ function getEnv(){
         env = document.getElementById('r2').value;
     }
     return env;
+}
+
+
+
+function printExperimentData(el, mass, env, time) {
+    var table = document.getElementById("table");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = el;
+    cell2.innerHTML = mass;
+    cell3.innerHTML = env;
+    cell4.innerHTML = time;
 }
