15da3a01890135d12e69c5ca825e4db21dff6fc8
santhosh bairam <santhubairamcs@gmail.com>
2015-06-15 10:16:26 +0530

    everything works fine... except speed eq & drop UX.

--- js/canvas.js
+++ js/canvas.js
@@ -1,15 +1,16 @@
 var imgObj = null;
 var imgObjRight = null;
-
+var speedRight=1;
+var speed=1;
 
 function init(){
     imgObj = document.getElementById('leftFinDestination');
-    imgObj.style.position= 'relative'; 
+//    imgObj.style.position= 'relative'; 
     imgObj.style.top = '15px';
     imgObjRight = document.getElementById('rightFinDestination');
-    imgObjRight.style.position= 'fixed'; 
-    imgObjRight.style.top = '0px';    
-    imgObjRight.style.right = '500px'; 
+//    imgObjRight.style.position= 'fixed'; 
+    imgObjRight.style.top = '15px';    
+//    imgObjRight.style.right = '500px'; 
 
 }
 function moveDown(){
@@ -22,10 +23,14 @@ function moveDown(){
         {
 //        console.log("obj: "+elementChildren(imgObj)[1].src.substring(30,elementChildren(imgObj)[1].src.length-4));
 //        console.log("mass: "+document.getElementById("sliderLeft").value);
-        console.log("env: "+getEnv());
+//        console.log("env: "+getEnv());
         var el=elementChildren(imgObj)[1].src.substring(30,elementChildren(imgObj)[1].src.length-4);
         var mass=document.getElementById("sliderLeft").value;
         printExperimentData(el, mass, getEnv(), 10);
+        document.getElementById("submit").value="Reset";
+        document.getElementById("submit").disabled=false;
+        document.getElementById('r1').disabled=false;
+        document.getElementById('r2').disabled=false;
         }
 }
 
@@ -39,13 +44,13 @@ function moveDownRight(){
 //        console.log(elementChildren(imgObjRight)[2].src.substring(30,elementChildren(imgObj)[1].src.length-4));
 //        console.log("mass: "+document.getElementById("sliderRight").value);
 //        console.log("env: "+getEnv());
-        var el=elementChildren(imgObjRight)[2].src.substring(30,elementChildren(imgObj)[1].src.length-4);
+        var el=elementChildren(imgObjRight)[1].src.substring(30,elementChildren(imgObj)[1].src.length-4);
+//        console.log("err: "+elementChildren(imgObjRight)[1]);
         var mass=document.getElementById("sliderRight").value;
         printExperimentData(el, mass, getEnv(), 10);
     }
 }
-var speedRight;
-var speed;
+
 window.onload =init;
 
 function dropObject(){
@@ -53,8 +58,10 @@ function dropObject(){
     speedRight=6;
     moveDown();
     moveDownRight();
+    document.getElementById('r1').disabled=true;
+    document.getElementById('r2').disabled=true;
     var btn=document.getElementById("submit");
-    btn.value="reset";
+    btn.disabled=true;
     btn.onclick=doRestore;
     
 }
@@ -64,20 +71,20 @@ function doRestore(){
     console.log("imgObj: "+imgObj);
     console.log(elementChildren(imgObj)[1]);
     console.log("woh: "+imgObj.childNodes[1][0]);
-    elementChildren(imgObj)[1].style.width="50px";
-    elementChildren(imgObj)[1].style.height="50px";
-    imgObjRight.style.width="50px";
-    imgObjRight.style.height="50px";
+//    elementChildren(imgObj)[1].style.width="50px";
+//    elementChildren(imgObj)[1].style.height="50px";
+//    imgObjRight.style.width="50px";
+//    imgObjRight.style.height="50px";
     if(elementChildren(imgObj)[1] || elementChildren(imgObjRight)[2])
         {
         document.getElementById('leftObjects').appendChild(elementChildren(imgObj)[1]);
-        document.getElementById('rightObjects').appendChild(elementChildren(imgObjRight)[2]);
+        document.getElementById('rightObjects').appendChild(elementChildren(imgObjRight)[1]);
         }
-    console.log(elementChildren(imgObjRight));
+    console.log("hel: "+elementChildren(imgObjRight));
 
     imgObj.style.top = '15px';
 
-    imgObjRight.style.top = '0px';
+    imgObjRight.style.top = '15px';
     var btn=document.getElementById("submit").value="Drop";
     document.getElementById("drag1").setAttribute("draggable", true);
     document.getElementById("drag2").setAttribute("draggable", true);
