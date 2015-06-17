de5f96b1406510ac82504694f83aad3882c441e1
santhosh bairam <santhubairamcs@gmail.com>
2015-06-17 11:09:09 +0530

    micro changes done...

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -17,11 +17,11 @@ function drop(ev) {
     if (data === "drag1" || data === "drag2" || data === "drag3" || data === "drag4"){
             console.log("in 1");
         ev.target.appendChild(document.getElementById(data));
-        var ob1=document.getElementById("drag1").setAttribute("draggable", false);  
-        var ob2=document.getElementById("drag2").setAttribute("draggable", false);
-        var ob3=document.getElementById("drag3").setAttribute("draggable", false);
-        var ob4=document.getElementById("drag4").setAttribute("draggable", false);
-        document.getElementById(data).setAttribute("draggable", true);
+//        var ob1=document.getElementById("drag1").setAttribute("draggable", false);  
+//        var ob2=document.getElementById("drag2").setAttribute("draggable", false);
+//        var ob3=document.getElementById("drag3").setAttribute("draggable", false);
+//        var ob4=document.getElementById("drag4").setAttribute("draggable", false);
+//        document.getElementById(data).setAttribute("draggable", true);
 //       var target=((data==="drag1")?"drag1":((data==="drag2")?"drag2":((data==="drag3")?"drag3":"drag4")));
 //    console.log("target: "+target);
 //    var ob5=document.getElementById(ev.target.id).setAttribute("draggable",true);
@@ -30,11 +30,11 @@ function drop(ev) {
     }
     else if (data === "drag11" || data === "drag22" || data === "drag33" || data === "drag44"){
         ev.target.appendChild(document.getElementById(data));
-        var ob11=document.getElementById("drag11").setAttribute("draggable", false);  
-        var ob22=document.getElementById("drag22").setAttribute("draggable", false);
-        var ob33=document.getElementById("drag33").setAttribute("draggable", false);
-        var ob44=document.getElementById("drag44").setAttribute("draggable", false);
-        document.getElementById(data).setAttribute("draggable", true);
+//        var ob11=document.getElementById("drag11").setAttribute("draggable", false);  
+//        var ob22=document.getElementById("drag22").setAttribute("draggable", false);
+//        var ob33=document.getElementById("drag33").setAttribute("draggable", false);
+//        var ob44=document.getElementById("drag44").setAttribute("draggable", false);
+//        document.getElementById(data).setAttribute("draggable", true);
     }
     else
     {;}
@@ -47,21 +47,31 @@ function drop(ev) {
     if(document.getElementById("leftDestination").childNodes[1]===undefined)
         {
         document.getElementById("sliderLeft").disabled=true;
-  //      document.getElementById("sliderLeft").value=0;
-        document.getElementById("leftMass").value=0;
         }
     else
+        {
         document.getElementById("sliderLeft").disabled=false;
+        document.getElementById("sliderLeft").value=100;
+        document.getElementById("leftMass").value=100;
+        }
     if(document.getElementById("rightDestination").childNodes[1]===undefined)
         {
         document.getElementById("sliderRight").disabled=true;
-//        document.getElementById("sliderRight").value=0;
-        document.getElementById("rightMass").value=0;
         }
     else
+        {
         document.getElementById("sliderRight").disabled=false;
-    
-        
+        document.getElementById("sliderRight").value=100;
+        document.getElementById("rightMass").value=100;
+        }
+    if(ev.target.id==="leftObjects")
+        {
+        document.getElementById(data).style.cssText="height: 50px; width: 50px;";
+        }
+    if(ev.target.id==="rightObjects")
+        {
+        document.getElementById(data).style.cssText="height: 50px; width: 50px;";
+        }
 }
 
 
